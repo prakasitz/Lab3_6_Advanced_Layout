e7770092030b95846eaefe3e7da722cc4a36ef46
prakasit <prakasitbm@gmail.com>
2018-09-23 20:19:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praka/Lab3_6_Advanced_Layout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.praka.Lab3_6_Advanced_Layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
